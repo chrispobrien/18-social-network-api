347d3e54022f8e031fe1c6b0adbf7c00666ae339
chrispobrien <chris.obrien57@gmail.com>
2022-08-12 11:08:15 -0400

    Added friend functionality to user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,6 +16,8 @@ const userController = {
     // get one user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
+            .populate({ path: 'thoughts', select: '-__v'})
+            .populate({ path: 'friends' })
             .select('-__v')
             .then(dbUserData => {
                 // if no user is found, send 404
@@ -49,6 +51,32 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(500).json(err));
+    },
+
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: params.friendId }}, {new: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
+    // delete a friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId }}, {new: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
     },
 
     // delete User by id
